f936ba40269058ea76338a0b9e8c24cc13f98788
Syeda Begum <begum.syeda2003@gmail.com>
2025-02-04 21:03:26 -0500

    changed Readme file

--- jobflow/app/layout.js
+++ jobflow/app/layout.js
@@ -6,12 +6,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <title>Jobflow</title>
+        <title>Job Flow</title>
       </Head>
       <body>
         <Navbar />
         <nav className="bg-blue-600 p-4 text-white">
-          <h1 className="text-2xl font-bold">Job Application Tracker</h1>
+          <h1 className="text-2xl font-bold"> Job Application Tracker</h1>
         </nav>
         <main className="p-4">{children}</main>
         <footer className="bg-gray-800 p-4 text-white text-center">
