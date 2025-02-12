493a39b28f260b90d01049682e42570001b8d2d7
perezc14-msu <perezc14@montclair.edu>
2025-02-12 10:24:28 -0500

    Added firebase js file

---
+++ jobflow/app/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCCem2lEn_s3viS6wZ9h-SoY_M6104UwnQ",
+  authDomain: "jobflow-291fc.firebaseapp.com",
+  projectId: "jobflow-291fc",
+  storageBucket: "jobflow-291fc.firebasestorage.app",
+  messagingSenderId: "54701827409",
+  appId: "1:54701827409:web:d1ff3452deb8b4f0a19f70",
+  measurementId: "G-B9G2ZBG86S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
