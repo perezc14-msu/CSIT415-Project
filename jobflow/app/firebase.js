4e13be6870c1059816a5123da673501f26553720
perezc14-msu <perezc14@montclair.edu>
2025-02-12 10:54:28 -0500

    Fixed firebase issue

--- jobflow/app/firebase.js
+++ jobflow/app/firebase.js
@@ -7,15 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCem2lEn_s3viS6wZ9h-SoY_M6104UwnQ",
-  authDomain: "jobflow-291fc.firebaseapp.com",
-  projectId: "jobflow-291fc",
-  storageBucket: "jobflow-291fc.firebasestorage.app",
-  messagingSenderId: "54701827409",
-  appId: "1:54701827409:web:d1ff3452deb8b4f0a19f70",
-  measurementId: "G-B9G2ZBG86S"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export{ app, analytics };
