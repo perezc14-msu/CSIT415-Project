f99a7df64097526a406ae9a587ac8e3e99f61263
kevinguerrero9 <guerrerok11@mail.montclair.edu>
2025-02-17 22:33:36 -0500

    Update Navbar.js
    job tracker top left changes

--- jobflow/app/components/Navbar.js
+++ jobflow/app/components/Navbar.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="bg-purple-600 p-4 text-white flex justify-between">
-      <h1 className="text-xl font-bold">Job Tracker</h1>
+      <h1 className="text-xl font-bold text-white bg-purple-500 p-3 rounded-lg">Job Tracker</h1>
       <div className="flex gap-4">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
