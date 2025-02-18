0ba278c875158796385a75cbe6f1af1cf92158c6
Syeda Begum <begum.syeda2003@gmail.com>
2025-02-18 00:32:21 -0500

    Fixed the Navbar layout and signIn and signup

--- jobflow/app/components/Navbar.js
+++ jobflow/app/components/Navbar.js
@@ -1,17 +1,27 @@
 "use client";
 
 import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className="bg-purple-600 p-4 text-white flex justify-between">
-      <h1 className="text-xl font-bold text-white bg-purple-500 p-3 rounded-lg">Job Tracker</h1>
+    <nav className="bg-purple-600 p-4 text-white flex justify-between items-center">
+      <h1 className="text-xl font-bold text-white bg-purple-500 p-3 rounded-lg">
+        Job Flow
+      </h1>
+
       <div className="flex gap-4">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/faq">FAQ</Link>
-        <Link href="/sign-in">Sign In</Link>
-        <Link href="/sign-up">Sign Up</Link>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton /> 
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton showName />
+        </SignedIn>
       </div>
     </nav>
   );
--- jobflow/app/layout.js
+++ jobflow/app/layout.js
@@ -11,21 +11,7 @@ export default function RootLayout({ children }) {
           <title><center>Job Flow</center></title>
         </Head>
         <body>
-          <Navbar />
-          <nav className="bg-purple-600 p-4 text-white">
-          <h1 className="text-5xl font-extrabold text-center bg-gradient-to-r from-purple-500 to-indigo-600 text-white p-4 rounded-lg shadow-lg">
-  Job Flow
-</h1>
-
-            <div>
-              <SignedOut>
-                <SignInButton />
-              </SignedOut>
-              <SignedIn>
-                <UserButton showName />
-              </SignedIn>
-            </div>
-          </nav>
+          <Navbar />   
           <main className="p-4">{children}</main>
           <footer className="bg-gray-800 p-4 text-white text-center">
             <p>© 2025 JobFlow</p>
