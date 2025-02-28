b91b02b72693f7f7728da457f5d00cc4eb1d93e1
kevinguerrero9 <guerrerok11@mail.montclair.edu>
2025-02-28 18:49:13 -0500

    UPDATED ABOUT PAGE
    underline about us, changed the font changed the width. The only thing left for the about us change would be linking it to the sign up page

--- jobflow/app/about/page.js
+++ jobflow/app/about/page.js
@@ -5,9 +5,15 @@ export default function About() {
   return (
     <div className="container">
       <h1 className="title">About Us</h1>
-      <p className="description">This app helps you track job applications efficiently.
-        sample text to test functionality</p>
-      <p className="description1">This is sample text</p>
+      <p className="description">Welcome to Jobflow, the ideal tool you need to 
+        keep track of all your job applications in one place. Job searching can be a hassle,
+         with numerous applications being completed each day, to the point you can no longer
+          recall what you even applied for. Our platform keeps track of your job, your interview
+           dates, and follow-ups. Whether you're a student, recent graduate, or someone looking for
+            a career switch, our platform is designed to simplify your job hunt. Sign up today and become
+             part of the Jobflow community!  
+      </p>
+     
     </div>
   );
 }
