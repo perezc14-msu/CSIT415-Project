c1416960fa59cbbdcecee14f7d977d9311701f3f
Syeda Begum <begum.syeda2003@gmail.com>
2025-04-02 23:07:09 -0400

    added logo in about apge

--- jobflow/app/about/page.js
+++ jobflow/app/about/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import "./about.css";
 
 export default function About() {
@@ -17,6 +18,14 @@ export default function About() {
   return (
     <div className="about-page">
       <div className="container">
+        {/* JobFlow Logo */}
+        <Image
+          src="/jobflow2.png" 
+          alt="JobFlow Logo"
+          width={80} 
+          height={80} 
+          className="mx-auto mb-4" 
+        />
         <h1 className="title">About Us</h1>
       </div>
 
@@ -26,8 +35,13 @@ export default function About() {
             Welcome to <strong>Jobflow</strong>, the ideal tool you need to keep track of all your job applications in one place.
             Job searching can be a hassle, with numerous applications being completed each day, to the point you
             can no longer recall what you even applied for. Our platform keeps track of your jobs, interview dates,
-            and follow-ups. Whether you're a student, recent graduate, or someone looking for a career switch, our
-            platform is designed to simplify your job hunt. Sign up today and become part of the Jobflow community!
+            and follow-ups. 
+            </p>
+            
+            <p className="text-lg text-gray-800 leading-relaxed mt-4">
+            Whether you're a student, recent graduate, or looking for a career switch, JobFlow ensures you stay
+            organized and ahead in your job search. Sign up today and become part of the JobFlow community!
+         
           </p>
         </div>
       )}
